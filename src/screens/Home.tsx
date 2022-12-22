229950dfd99c65255c6230d0a5962fb4d48b39c4
Ptdavid0 <pedro_antonio.pereira@syngenta.com>
2022-12-22 13:12:19 -0300

    Home Style finished

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,59 @@
+import ExerciseCard from "@components/ExerciseCard";
 import HomeHeader from "@components/HomeHeader";
-import { Center, VStack } from "native-base";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 import React from "react";
+import Group from "../components/Group";
 
 const Home: React.FC = () => {
+  const [activeGroup, setActiveGroup] = React.useState("costas");
+  const [exercises, setExercises] = React.useState([
+    "Puxada Frontal",
+    "Remada lateral",
+    "Remada unilateral",
+    "Remada baixa",
+    "Remada alta",
+  ]);
+
+  const isCurrentActiveGroup = (name: string) =>
+    activeGroup.toLocaleUpperCase() === name.toLocaleUpperCase();
+
   return (
     <VStack flex={1}>
       <HomeHeader />
+      <FlatList
+        data={["costas", "peito", "bíceps", "tríceps", "ombros", "pernas"]}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={isCurrentActiveGroup(item)}
+            onPress={() => setActiveGroup(item)}
+          />
+        )}
+        keyExtractor={(item) => item}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          renderItem={({ item }) => <ExerciseCard name={item} />}
+          keyExtractor={(item) => item}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 };
